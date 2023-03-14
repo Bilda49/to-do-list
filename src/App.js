0d5280051836504d00d8698c681ff289c8f5e014
Bilda49 <gridlock2001@gmail.com>
2023-03-14 09:46:12 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [todoList, setTodoList] = useState([]);
+  const [editingTaskId, setEditingTaskId] = useState(null);
+  const [editedTaskValue, setEditedTaskValue] = useState("");
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleEditInputChange = (event) => {
+    setEditedTaskValue(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    if (inputValue.trim() !== "") {
+      setTodoList([
+        ...todoList,
+        { id: Date.now(), task: inputValue, completed: false },
+      ]);
+      setInputValue("");
+    }
+  };
+
+  const handleTaskDelete = (id) => {
+    setTodoList(todoList.filter((task) => task.id !== id));
+  };
+
+  const handleTaskCompletion = (id) => {
+    setTodoList(
+      todoList.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed: !task.completed };
+        }
+        return task;
+      })
+    );
+  };
+  const handleEditFormSubmit = (event) => {
+    event.preventDefault();
+    setTodoList(
+      todoList.map((task) => {
+        if (task.id === editingTaskId) {
+          return { ...task, task: editedTaskValue };
+        }
+        return task;
+      })
+    );
+    setEditingTaskId(null);
+    setEditedTaskValue("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <form onSubmit={handleFormSubmit}>
+        <input type="text" value={inputValue} onChange={handleInputChange} />
+        <button type="submit">Add Task</button>
+      </form>
+      <ul>
+        {todoList.map((task) => (
+          <li key={task.id}>
+            {editingTaskId === task.id ? (
+              <form onSubmit={handleEditFormSubmit}>
+                <input
+                  type="text"
+                  value={editedTaskValue}
+                  onChange={handleEditInputChange}
+                />
+                <button type="submit">Save</button>
+              </form>
+            ) : (
+              <>
+                <span className={task.completed ? "completed" : ""}>
+                  {task.task}
+                </span>
+                <button onClick={() => handleTaskDelete(task.id)}>
+                  Delete
+                </button>
+                <button onClick={() => handleTaskCompletion(task.id)}>
+                  {task.completed ? "Incomplete" : "Complete"}
+                </button>
+                <button onClick={() => setEditingTaskId(task.id)}>Edit</button>
+              </>
+            )}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
